655f3dbb3123cbfa90eb7b488e23c13414e5d176
Eric Todd <erictoddcreative@gmail.com>
2022-03-24 22:58:51 -0400

    Update mail.php

--- assets/ajax/mail.php
+++ assets/ajax/mail.php
@@ -6,7 +6,7 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 
 	$message = "First name: $first_name\nLast name: $last_name\nEmail: $email\nMessage: $message";
-	$subject = "=?utf-8?B?".base64_encode("message from the site")."?=";
+	$subject = "=?utf-8?B?".base64_encode("Message From Eric Todd Creative Site")."?=";
 	$headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/plain; charset=utf-8\r\n";
 
 	if(mail($to, $subject, $message, $headers))
